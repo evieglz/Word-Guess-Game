2971d03a88088f42a95f8d70edd6ea3ffcb09aac
Evelyn Gonzalez <evieglz369@gmail.com>
2018-07-27 21:42:03 -0500

    updated stylesheet and javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,8 @@
 // start with choosing a theme: FRUITS 
     // apples, bannanas, kiwis, pinapples, mangos
-    var fruits=["apples", "bannanas", "kiwis", "pineapples","mangos"];
+var rockBands=["queen","AC/DC", "nirvana", "metallica", "journey", "muse", "coldplay", "incubus"];
+
+
 
 // start with a variable of 0 for win
 var win= 0;
@@ -11,30 +13,59 @@ var loss=0;
 // start with 6 guess (move down)
 var guessesRemaining=6;
 
+// create a var for stage.
+var stage=" ";
+var text= " ";
+
+// createa a var for "guess correctly"
+    // guess correctly will be a boolean false
+
+var guessCorrectly= false;
 
 // create a generator where the words will appear
-document.querySelector("#stage").innerHTML=" "
+document.querySelector("#stage").innerHTML=stage;
 
-document.querySelector("#guess-counter").innerHTML=stage;
-var random = fruits[Math.floor(Math.random() * fruits.length)]
-
+// for loop for fruits and an array for underscores
+var random = rockBands[Math.floor(Math.random() * rockBands.length)]
+var underscores = [];
 console.log(random)
 
-var stage = " "
-function generator (str){
-  for (char in str){
-    stage = stage + " _ "
-  }
-  console.log(stage)
+// replaces the underscore array with "_"
+for (var i=0; i<random.length; i++){
+    underscores[i] = "_"
+    
+    console.log(underscores)
 }
-    // have it link with the wordlist=fruits
-        // create a function that will render the random words from FRUITS
+document.querySelector("#stage").innerHTML=underscores;
 
-// connect win with #win from HTML
-    // create a function to count the wins
+  
+// create inner HTML for win/losses/guesses
+document.querySelector("#win").innerHTML=("win: "+ win);
+// make them split up
+document.querySelector("#losses").innerHTML="loss: " +loss;
+// make them split up
+document.querySelector("#guess-counter").innerHTML= ("guesses: " + guessesRemaining);
 
-// connect loss with #loss from HTML
-    // create a function to count the loss
+// create a function for onkeyup and a var for function
+    document.onkeyup = function(event) {
+        var userGuess = event.key;
+       
+        for (var i=0; i< random.length; i++) {
+            if (random[i]== userGuess){
+                console.log("this works");
+                underscores[i]=userGuess;
+                console.log(underscores);
+                guessCorrectly=true;
+            }
+            else { 
+                console.log("wrong");
+            }
+            
+            
+
+        }
+        
+    };
 
 
     // MAIN PROCESS
@@ -48,3 +79,5 @@ function generator (str){
     // once the user runs out of 6 lives, it's game over. 
 
     // if they guess correctly, they win the game. 
+
+    // BONUS: add an evil laugh if they loose or cheers if they win. 
